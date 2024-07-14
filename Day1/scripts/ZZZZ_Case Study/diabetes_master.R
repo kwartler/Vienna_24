4fa64c21b5776216e7bafd69c802658ed7482e91
Kwartler <you@example.com>
2024-07-13 23:02:44 -0400

    add case study scripts

--- Day1/scripts/ZZZZ_Case Study/diabetes_master.R
+++ Day1/scripts/ZZZZ_Case Study/diabetes_master.R
@@ -1,6 +1,21 @@
 #' Author: Ted Kwartler
 #' Date: 6-30-2024
-#' Purpose: Case Study - 
+#' Purpose: Case Study - Obtain, Scrub, Explore, Model, Interpret
 #' 
 
+# libraries
+
+# Obtain the data - find the raw data on https://github.com/kwartler/teaching-datasets/tree/main/diabetes_tables
+
+# Join the data
+
+# Scrub the data - account for missing variables
+
+# Create a DTM from the drNotes column
+
+# Explore the data
+
+# Arrange into a modeling matrix with both original data and the DTM & save a copy
+
+# Success for day 1 is a modeling matrix that has been cleaned meaning no missing data, dummy variables have been created, the DTM is represented (cbind) and there are one or more engineered variables
 # End
--- Day1/scripts/ZZZZ_Case Study/diabetes_mid.R
+++ Day1/scripts/ZZZZ_Case Study/diabetes_mid.R
@@ -1,6 +1,27 @@
 #' Author: Ted Kwartler
 #' Date: 6-30-2024
-#' Purpose: Case Study - 
+#' Purpose: Case Study - Obtain, Scrub, Explore, Model, Interpret
 #' 
+
+# Libraries 
+
+# Obtain the data
+drNotes <- read.csv('https://raw.githubusercontent.com/kwartler/teaching-datasets/main/diabetes_tables/drNotes.csv')
+drugData <- read.csv('https://raw.githubusercontent.com/kwartler/teaching-datasets/main/diabetes_tables/drugData.csv')
+hospitalStayData <- read.csv('https://raw.githubusercontent.com/kwartler/teaching-datasets/main/diabetes_tables/hospitalStayData.csv')
+patientDemographic <- read.csv('https://raw.githubusercontent.com/kwartler/teaching-datasets/main/diabetes_tables/patientDemographics.csv')
+
+# Join the data
+# Join in this order to simplicity
+# patientDemographic>drugData>hospitalStayData>drNotes
+
+# Scrub the data - account for missing with dummy variables; drop the dr notes section if you joined it; an easy method after the join is to explore the library(vtreat) or use the various dummy packages to help you; and remember you can use GPT!
+
+
+# Explore the data - create a lot of visuals and summary statistics understanding aspects of the data, correlations, frequencies etc
+
+# Arrange into a modeling matrix of relevant data inputs and the y variable & save a copy
+
+# Success for day 1 is a modeling matrix that has been cleaned meaning no missing data and dummy variables have been created where appropriate
 
 # End
