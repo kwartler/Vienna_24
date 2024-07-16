2c8a3b3c290c236eb0a77e205913649dc7a0fed4
Kwartler <you@example.com>
2024-07-16 15:14:57 -0400

    update post mod assignment

---
+++ PostModuleAssignment/Student_scaffold.R
@@ -0,0 +1,27 @@
+#' Vienna Post Module Assignment
+#' TK
+#' July 16
+#' Obtain, Scrub, Explore, Model, Assess
+#' 
+#' 
+
+# Libraries
+library(vtreat)
+
+## Obtain the data
+#current table: https://raw.githubusercontent.com/kwartler/Vienna_July24/main/PostModuleAssignment/training/CurrentCustomerMktgResults.csv
+# Axiom table: https://raw.githubusercontent.com/kwartler/Vienna_July24/main/PostModuleAssignment/training/householdAxiomData.csv
+# HH Credit table: https://github.com/kwartler/Vienna_July24/blob/main/PostModuleAssignment/training/householdCreditData.csv
+# HH Vehicle table: https://raw.githubusercontent.com/kwartler/Vienna_July24/main/PostModuleAssignment/training/householdVehicleData.csv
+
+## Explore - visuals, summary stats, correlation, get to know the data and challenge yourself to think about the data (there is a small trick betwen the prospect and training data variables, see if you can find it)
+
+
+## Scrub - vtreat package & possibly engineer features, remove outliers etc.
+
+## Model - create a parsimonious logistic regression
+
+## Assess - create a confusion matrix and measure the model's accuracy after choosing a cutoff threshold.
+
+# End
+
