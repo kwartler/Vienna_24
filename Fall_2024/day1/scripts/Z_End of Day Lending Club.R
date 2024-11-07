12007fc4bb1b9510a6267ff8102864651fe66c88
Kwartler <you@example.com>
2024-11-07 09:34:39 +0100

    add custom function

--- Fall_2024/day1/scripts/Z_End of Day Lending Club.R
+++ Fall_2024/day1/scripts/Z_End of Day Lending Club.R
@@ -8,6 +8,21 @@
 
 #### Working Directory
 
+#### Custom Functions
+removePercentSign <- function(df, colNamesToFix){
+  # select the columns to fix
+  x <- df[,colNamesToFix]
+  
+  # remove the % sign, and make numeric
+  for(i in 1:ncol(x)){
+    fixedCol <- as.numeric(gsub("%","",x[,i]))/100
+    x[,i]    <- fixedCol
+  }
+  
+  finalDF <- df %>% select(-colNamesToFix)
+  finalDF <- cbind(finalDF, x)
+}
+
 #### Data Load - your dependent variable is "y" which is a binary output; 1 is success 0 is a default
 loans <- read.csv('https://github.com/kwartler/Vienna_24/raw/refs/heads/main/Fall_2024/day1/data/20K_sampleLoans.csv')
 investmentOpportunities <- read.csv('https://raw.githubusercontent.com/kwartler/Vienna_24/refs/heads/main/Fall_2024/day1/data/OpenNotesJune18_v2.csv')
